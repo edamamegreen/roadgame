b2839dc8c7ba80e9a170fbafd109174c0bb6a77e
edamamegreen <nicki@boxingindustries.com>
2017-05-06 21:18:56 -0700

    Tidied up game screen elements. Console prints an obstacle warning when car crosses into a vertical zone of the screen.

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 
 var distIncrement = 10;
+var radix = 10;
+var obstacle = 240;
+var carLeftString;
+var carLeftX;
 
-function carLeft() {
-	var carPosition = window.getComputedStyle(document.querySelector('.car')).getPropertyValue('left');
-	document.querySelector('.car').style.left = (parseInt(carPosition, 10) - distIncrement) + "px";
+function calcLeftX() {
+	carLeftX = parseInt(carLeftString, radix);
+	// console.log(carLeftX);
+}
+
+function moveCarLeft() {
+	document.querySelector('.car').style.left = (carLeftX - distIncrement) + "px";
 } 
 
-function carRight() {
-	var carPosition = window.getComputedStyle(document.querySelector('.car')).getPropertyValue('left');
-	document.querySelector('.car').style.left = (parseInt(carPosition, 10) + distIncrement) + "px";
+function moveCarRight() {
+	document.querySelector('.car').style.left = (carLeftX + distIncrement) + "px";
 } 
+
+function checkOverlap() {
+	if ((carLeftX > (obstacle - 50)) && (carLeftX < (obstacle + 50))) {
+		console.log('Obstacle!');
+	}
+	// console.log(carLeftX);
+}
 
 function leftRight(e) {
 
 	if (e.keyCode == 37) {
 		// the left arrow has been pressed
-		carLeft();
+		carLeftString = window.getComputedStyle(document.querySelector('.car')).getPropertyValue('left');
+		calcLeftX();
+		moveCarLeft();
+		checkOverlap();
 	}
 	if (e.keyCode == 39) {
 		// the right arrow has been pressed
-		carRight();
+		carLeftString = window.getComputedStyle(document.querySelector('.car')).getPropertyValue('left');
+		calcLeftX();
+		moveCarRight();
+		checkOverlap();
 	}
 }
 
 var keyListener = document.querySelector('body');
 keyListener.addEventListener('keydown', leftRight, false);
+
+
+
+
+
+
