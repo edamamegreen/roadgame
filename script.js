c4a4e94cfcf287a93f18fc33196870033da9e799
edamamegreen <nicki@boxingindustries.com>
2017-05-07 16:21:07 -0700

    Updated page description. Added a function to move the obstacles I create and remove them when they reach the bottom of the road view. Currently I am manually creating obstacles by submitting makeObstacle(); in the console.

--- script.js
+++ script.js
@@ -52,20 +52,42 @@ var gameWidth = parseInt(window.getComputedStyle(document.querySelector('.road')
 // Trying to create a function that adds a small div
 // into the road, randomly along its width.
 
-function randoShit () {
+function makeObstacle () {
+	// An obstacle is created at the top of the road at a random x position.
 	var placeIt = Math.round(Math.random()*gameWidth);
 	var newDiv = document.createElement("div");
 	var objParent = document.querySelector('.road');
 	newDiv.classList.add("cow");
-	newDiv.style.backgroundColor = "lime";
-	newDiv.style.position = "absolute";
-	newDiv.style.width = 50 + "px";
-	newDiv.style.height = 50+ "px";
 	newDiv.style.top = 10 + "px";
 	newDiv.style.left = placeIt + "px";
 	objParent.insertBefore(newDiv, document.querySelector('.car'));
+	moveObstacles();
 }
 
+function moveObstacles () {
+	// The obstacle moves down the road and disappears when it reaches the
+	// bottom of the road.
+	// I chose to do this in JS instead of CSS animate because I want
+	// to be able to remove the element when it finishes moving.
+	var elem = document.querySelector('.cow');
+	var obstacleHeight = parseInt(window.getComputedStyle(elem).getPropertyValue('height'), radix);
+	var pos = parseInt(window.getComputedStyle(elem).getPropertyValue('top'), radix);
+	var roadHeight = parseInt(window.getComputedStyle(document.querySelector('.road')).getPropertyValue('height'), radix);
+	if (pos < (roadHeight - obstacleHeight)) {
+		// move down a step
+		elem.style.top = pos + 2 + "px";
+		console.log(elem.style.top);
+		requestAnimationFrame(moveObstacles);
+	} else {
+		// remove from DOM
+		document.querySelector('.road').removeChild(elem);
+	}
+}
 
-
-
+// setInterval(makeObstacle, 1000);
+// When I turn on setInterval, moveObstacles seems to 
+// speed up when its moving more than one element.
+// When multiple obstacles are on the screen,
+// they begin to pause before moving.
+// I'm not sure why. 
+// How can I investigate this phenomenon?
