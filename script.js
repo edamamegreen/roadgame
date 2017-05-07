5a31977a7fb7c8203969c71f5c55af90faeb4997
edamamegreen <nicki@boxingindustries.com>
2017-05-06 17:01:19 -0700

    Implemented move car left/right with left/right arrow keys. Collects computed left position, parses for integer, increments or decrements and changes the style left position.

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
 
-function setCarPosition(x) {
-	document.querySelector('.car').style.left = x + "px";
+var distIncrement = 10;
+
+function carLeft() {
+	var carPosition = window.getComputedStyle(document.querySelector('.car')).getPropertyValue('left');
+	document.querySelector('.car').style.left = (parseInt(carPosition, 10) - distIncrement) + "px";
+} 
+
+function carRight() {
+	var carPosition = window.getComputedStyle(document.querySelector('.car')).getPropertyValue('left');
+	document.querySelector('.car').style.left = (parseInt(carPosition, 10) + distIncrement) + "px";
 } 
 
 function leftRight(e) {
-	console.log(e);
-	if (e.keycode == 27) {
+
+	if (e.keyCode == 37) {
 		// the left arrow has been pressed
-		console.log(e);
-		console.log('left');
+		carLeft();
 	}
-	if (e.keycode == 26) {
+	if (e.keyCode == 39) {
 		// the right arrow has been pressed
-		console.log(e);
-		console.log('right');
+		carRight();
 	}
 }
-
-// function tabClick(e) {
-// 	if (e.keycode == 33) {
-// 		// the left arrow has been pressed
-// 		console.log(e);
-// 		console.log('left');
-// 	}
-// 	if (e.keycode == 26) {
-// 		// the right arrow has been pressed
-// 		console.log('right');
-// 	}
-// }
-
-// var tabElement = document.querySelector('.gamebox');
-// tabElement.addEventListener('keypress', tabClick, false);
 
 var keyListener = document.querySelector('body');
 keyListener.addEventListener('keydown', leftRight, false);
