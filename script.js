60499a50d34a33ecb82843b6a588410c2fc22fd2
edamamegreen <nicki@boxingindustries.com>
2017-05-14 16:24:51 -0700

    Started restructuring game to have states: Intro, Play, Crash. The game starts in Play state now and doesn’t transition to other states yet. moveObstacles now grabs all obstacle divs as an array and loops through the array to increment each of them forward.

--- script.js
+++ script.js
@@ -5,10 +5,108 @@ var obstacle = 240;
 var carLeftString;
 var carLeftX;
 
+
+
+// Define the states
+// Listen for spacebar to move into play state
+// Play: Create, move objects and check for collisions, listen for esc to move to intro state
+// When a collision occurs animate crash and return to intro state
+
+var gameState = "play";
+
+
+// ADD OBSTACLES
+// Create a small div placed into the road, randomly along the road's width.
+
+var gameWidth = parseInt(window.getComputedStyle(document.querySelector('.road')).getPropertyValue('width'), radix);
+
+function makeObstacle () {
+	// An obstacle is created at the top of the road at a random x position.
+	var placeIt = Math.round(Math.random()*gameWidth);
+	var newDiv = document.createElement("div");
+	var objParent = document.querySelector('.road');
+	newDiv.classList.add("cow");
+	newDiv.style.top = 10 + "px";
+	newDiv.style.left = placeIt + "px";
+	objParent.insertBefore(newDiv, document.querySelector('.car'));
+}
+
+function moveRoad () {
+
+}
+
+function moveCar () {
+
+}
+
+function moveObstacles () {
+	// The obstacle moves down the road and disappears when it reaches the
+	// bottom of the road.
+	// I chose to do this in JS instead of CSS animate because I want
+	// to be able to remove the element when it finishes moving.
+	var elem = document.querySelectorAll('.cow');
+	var roadHeight = parseInt(window.getComputedStyle(document.querySelector('.road')).getPropertyValue('height'), radix);
+	
+	for (var i = elem.length - 1; i >= 0; i--) {
+
+		var obstacleHeight = parseInt(window.getComputedStyle(elem[i]).getPropertyValue('height'), radix);
+		var pos = parseInt(window.getComputedStyle(elem[i]).getPropertyValue('top'), radix);	
+		
+		if (pos < (roadHeight - obstacleHeight)) {
+		// move down a step
+		elem[i].style.top = pos + 2 + "px";
+		console.log(elem[i].style.top);
+		} else {
+			// remove from DOM
+			document.querySelector('.road').removeChild(elem[i]);
+			console.log(elem[i]);
+		}	
+	}
+		requestAnimationFrame(moveObstacles);
+}
+
+function checkCollision () {
+
+}
+
+function stepGame() {	
+	moveObstacles();
+	// Adding the rest of these functions creates an Uncaught RangeError: Maximum call stack size exceeded
+	// It's definitely not a good idea to be using requestAnimationFrame in both stepGame and moveObstacles
+	// and then to call moveObstacles within stepGame.
+	// requestAnimationFrame(moveRoad, moveCar, moveObstacles);
+	// checkCollision();
+	// stepGame();
+}
+
+if (gameState == "play") {
+	// createCar();
+	stepGame();
+}
+
+
+
+// CHECK FOR COLLISION
+
+function checkOverlap() {
+	if ((carLeftX > (obstacle - 50)) && (carLeftX < (obstacle + 50))) {
+		console.log('Obstacle!');
+	}
+	// console.log(carLeftX);
+}
+
+
+
+// MOVE THE CAR
+// get the numeric value of the left position of the car
+
 function calcLeftX() {
 	carLeftX = parseInt(carLeftString, radix);
 	// console.log(carLeftX);
 }
+
+
+// move the car by incrementing or decrementing the car's left position
 
 function moveCarLeft() {
 	document.querySelector('.car').style.left = (carLeftX - distIncrement) + "px";
@@ -18,12 +116,7 @@ function moveCarRight() {
 	document.querySelector('.car').style.left = (carLeftX + distIncrement) + "px";
 } 
 
-function checkOverlap() {
-	if ((carLeftX > (obstacle - 50)) && (carLeftX < (obstacle + 50))) {
-		console.log('Obstacle!');
-	}
-	// console.log(carLeftX);
-}
+// has a left/right arrow key been pressed? then move the car
 
 function leftRight(e) {
 
@@ -47,44 +140,13 @@ var keyListener = document.querySelector('body');
 keyListener.addEventListener('keydown', leftRight, false);
 
 
-var gameWidth = parseInt(window.getComputedStyle(document.querySelector('.road')).getPropertyValue('width'), radix);
 
-// Trying to create a function that adds a small div
-// into the road, randomly along its width.
 
-function makeObstacle () {
-	// An obstacle is created at the top of the road at a random x position.
-	var placeIt = Math.round(Math.random()*gameWidth);
-	var newDiv = document.createElement("div");
-	var objParent = document.querySelector('.road');
-	newDiv.classList.add("cow");
-	newDiv.style.top = 10 + "px";
-	newDiv.style.left = placeIt + "px";
-	objParent.insertBefore(newDiv, document.querySelector('.car'));
-	moveObstacles();
-}
 
-function moveObstacles () {
-	// The obstacle moves down the road and disappears when it reaches the
-	// bottom of the road.
-	// I chose to do this in JS instead of CSS animate because I want
-	// to be able to remove the element when it finishes moving.
-	var elem = document.querySelector('.cow');
-	var obstacleHeight = parseInt(window.getComputedStyle(elem).getPropertyValue('height'), radix);
-	var pos = parseInt(window.getComputedStyle(elem).getPropertyValue('top'), radix);
-	var roadHeight = parseInt(window.getComputedStyle(document.querySelector('.road')).getPropertyValue('height'), radix);
-	if (pos < (roadHeight - obstacleHeight)) {
-		// move down a step
-		elem.style.top = pos + 2 + "px";
-		console.log(elem.style.top);
-		requestAnimationFrame(moveObstacles);
-	} else {
-		// remove from DOM
-		document.querySelector('.road').removeChild(elem);
-	}
-}
 
-// setInterval(makeObstacle, 1000);
+
+
+setInterval(makeObstacle, 2000);
 // When I turn on setInterval, moveObstacles seems to 
 // speed up when its moving more than one element.
 // When multiple obstacles are on the screen,
